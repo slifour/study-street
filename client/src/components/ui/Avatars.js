ef70e693130147a56f01f5e769733c79acee1498
Han Jeonghoon <gkswjdgns59@kaist.ac.kr>
2021-11-02 02:25:51 +0900

    trivial2

---
+++ client/src/components/ui/Avatars.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import SingleAvatar from './SingleAvatar';
+
+export default function Avatars() {
+    
+
+    return(
+        <div>
+            <SingleAvatar userId={"eunki"}></SingleAvatar>
+        </div>
+    )
+}
--- client/src/components/ui/SingleAvatar.js
+++ client/src/components/ui/SingleAvatar.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import socket from '../../../socket';
 
-export default function SingleAvatar(userId) {
+export default function SingleAvatar(props) {
+    
+    useEffect(() => {
+        socket.emit("userProfileRequest", props.userId);
+    }, [])
+
+    socket.on("userProfile", (userInfo) => {
+        console.log(userInfo)
+    })
+    
     return (
         <div>
             
