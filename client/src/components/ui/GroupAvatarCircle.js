288c14bb6e26f8862eeed1f0c3ba271c44c00dc7
Eunki <ppyapmail@gmail.com>
2021-11-24 20:53:01 +0900

    add : show group icon in library scene

--- client/src/components/ui/GroupAvatarCircle.js
+++ client/src/components/ui/GroupAvatarCircle.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import styles from "./home.module.css";
+
+const StyledHoverLabelWrapper = styled.div`
+    .hoverLabel { 
+        position: absolute;
+        font-weight: 800;
+        opacity: 0;
+        top: ${props => -(props.size || 60)}px;
+    }
+    &:hover .hoverLabel {
+        opacity: 1;
+    }
+`;
 
 const StyledAvatar = styled.div`
     box-shadow: 0px 4px 4px rgba(88, 88, 88, 0.25);
@@ -8,23 +19,28 @@ const StyledAvatar = styled.div`
     color: ${props => props.color};
     text-align: center;
     font-weight: 600;
-    cursor: pointer;
+    cursor: ${props => props.onClick ? "pointer" : "default"};
     background: ${props => props.background};
 `;
 
-export default function GroupAvatarCircle({group, size}) {
+export default function GroupAvatarCircle({group, size, onMouseEnterItem, onClick}) {
     
     size = size || 60; // default size in px
     const fontSize = size / 2.5;
     const lineHeight = size;
-    const shortID = group.groupID.substr(0, 2).toUpperCase();
+    const displayName = group.groupName.substr(0, 2).toUpperCase();
     const defaultColor = "#ffffff";
     const defaultBackground = "#00bc36"
 
     return (
         <div>
-            { 
+            <StyledHoverLabelWrapper
+                style={{
+                    width: `${size}px`
+                }}>
+                <p className="hoverLabel">{onMouseEnterItem}</p>
                 <StyledAvatar 
+                    onClick={onClick}
                     color = {group.colors ? group.colors[0] : defaultColor} 
                     background = {group.colors ? group.colors[1] : defaultBackground} 
                     style={{
@@ -34,9 +50,9 @@ export default function GroupAvatarCircle({group, size}) {
                         fontSize: `${fontSize}px`
                     }
                 }>
-                    {shortID}
+                    {displayName}
                 </StyledAvatar>
-            }
+            </StyledHoverLabelWrapper>
         </div>
     )
 }
